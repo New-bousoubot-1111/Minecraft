ff1ee483d37d6041f1342279376cc9c3b949c0af
New-bousoubot-1111 <127783879+New-bousoubot-1111@users.noreply.github.com>
2025-01-03 23:56:37 +0900

    Update set_panel.js

--- src/interactions/panel/set_panel.js
+++ src/interactions/panel/set_panel.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, channelMention } = require('discord.js');
+const { EmbedBuilder, channelMention, MessageFlags } = require('discord.js');
 const { panels, lang } = require('../../index.js');
 
 /** @param {import('discord.js').ChatInputCommandInteraction} interaction */
@@ -6,15 +6,15 @@ async function setPanel(interaction) {
   const embed = new EmbedBuilder()
     .setAuthor({ name: 'Status Panel' })
     .setDescription([
-      lang.run('command.panel.set', [ channelMention(interaction.channelId) ])
-    ].join('\n'))
-  await interaction.reply({ embeds: [embed], ephemeral: true });
+      lang.run('command.panel.set', [channelMention(interaction.channelId)])
+    ].join('\n'));
+  
+  await interaction.reply({ embeds: [embed], flags: MessageFlags.Ephemeral });
   
   // パネル作成, 保存
   await panels.create(interaction.channelId);
   
   await panels.update();
 }
-
 
 module.exports = setPanel;
