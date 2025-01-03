b6878b5ef0a7453b904459a87750765492ef11fa
New-bousoubot-1111 <127783879+New-bousoubot-1111@users.noreply.github.com>
2025-01-03 22:52:40 +0900

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,5 @@
 require('dotenv').config(); // 環境変数を読み込むための設定
-
-const discord_token = process.env.discord_token;
 const { server, client, version } = require('discord-mcbe');
-
-if (!discord_token) {
-  console.error('Error: Discord token is not defined in environment variables.');
-  process.exit(1); // トークンがない場合はエラーで終了
-}
 
 console.log('[Script] loaded!');
 
